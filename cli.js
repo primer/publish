bc4165b0e9ca655b2023177faf05233003039c39
Shawn Allen <shawn.allen@github.com>
2019-02-06 11:32:14 -0800

    chore: remove --message option from CLI

--- cli.js
+++ cli.js
@@ -1,16 +1,10 @@
 #!/usr/bin/env node
 const publish = require('./src/publish')
-const {DEFAULT_MESSAGE} = require('./src/constants')
 
 const yargs = require('yargs')
   .option('dry-run', {
     describe: 'Print what will be done without doing it',
     type: 'boolean'
-  })
-  .option('message', {
-    describe: 'The git commit message (not pushed)',
-    alias: 'm',
-    default: DEFAULT_MESSAGE
   })
 
 const options = yargs.argv
