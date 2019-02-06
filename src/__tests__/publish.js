55ee6f4e6b88083c352c74ef6088a45fd27209f8
Shawn Allen <shawn.allen@github.com>
2019-02-05 23:37:38 -0800

    refactor: getContext() sync, etc.

---
+++ src/__tests__/publish.js
@@ -0,0 +1,116 @@
+const execa = require('execa')
+const mockedEnv = require('mocked-env')
+const publish = require('../publish')
+const readJSON = require('../read-json')
+const {mockFiles} = require('./__utils')
+
+jest.mock('execa')
+jest.mock('../read-json')
+
+describe('publish()', () => {
+  let restoreEnv = () => {}
+
+  beforeEach(() => {
+    execa.mockImplementation(() => Promise.resolve({stdout: '', stderr: ''}))
+  })
+
+  afterEach(() => {
+    restoreEnv()
+    execa.mockReset()
+    readJSON.mockReset()
+  })
+
+  it('throws if NPM_AUTH_TOKEN is falsy', () => {
+    mockEnv({NPM_AUTH_TOKEN: undefined})
+    expect(() => publish()).toThrow()
+    mockEnv({NPM_AUTH_TOKEN: ''})
+    expect(() => publish()).toThrow()
+  })
+
+  it('does the right things on a feature branch', () => {
+    mockEnv({
+      GITHUB_REF: 'refs/heads/feature-x',
+      GITHUB_SHA: 'deadfad',
+      NPM_AUTH_TOKEN: 'secret'
+    })
+    mockFiles({
+      'package.json': {name: 'pkg', version: '1.0.0'}
+    })
+    const version = '0.0.0-sha.deadfad'
+    return publish().then(() => {
+      expect(execa).toHaveBeenCalledTimes(3)
+      expect(execa).toHaveBeenNthCalledWith(1, 'npm', ['version', version])
+      expect(execa).toHaveBeenNthCalledWith(2, 'git', [
+        'commit',
+        '-m',
+        `chore: npm version ${version}`,
+        'package*.json'
+      ])
+      expect(execa).toHaveBeenNthCalledWith(3, 'npm', ['publish', '--tag', 'canary', '--access', 'public'])
+    })
+  })
+
+  it('does the right things on a release branch', () => {
+    mockEnv({
+      GITHUB_REF: 'refs/heads/release-2.0.0',
+      GITHUB_SHA: 'deadfad',
+      NPM_AUTH_TOKEN: 'secret'
+    })
+    mockFiles({
+      'package.json': {name: 'pkg', version: '1.0.0'}
+    })
+    const version = '2.0.0-next.deadfad'
+    return publish().then(() => {
+      expect(execa).toHaveBeenCalledTimes(3)
+      expect(execa).toHaveBeenNthCalledWith(1, 'npm', ['version', version])
+      expect(execa).toHaveBeenNthCalledWith(2, 'git', [
+        'commit',
+        '-m',
+        `chore: npm version ${version}`,
+        'package*.json'
+      ])
+      expect(execa).toHaveBeenNthCalledWith(3, 'npm', ['publish', '--tag', 'next', '--access', 'public'])
+    })
+  })
+
+  it('does the right things on master', () => {
+    mockEnv({
+      GITHUB_REF: 'refs/heads/master',
+      GITHUB_SHA: 'deadfad',
+      NPM_AUTH_TOKEN: 'secret'
+    })
+    mockFiles({
+      'package.json': {name: 'pkg', version: '1.1.0'}
+    })
+    const version = '1.1.0'
+    return publish().then(() => {
+      expect(execa).toHaveBeenCalledTimes(3)
+      expect(execa).toHaveBeenNthCalledWith(1, 'npm', ['version', version])
+      expect(execa).toHaveBeenNthCalledWith(2, 'git', [
+        'commit',
+        '-m',
+        `chore: npm version ${version}`,
+        'package*.json'
+      ])
+      expect(execa).toHaveBeenNthCalledWith(3, 'npm', ['publish', '--tag', 'latest', '--access', 'public'])
+    })
+  })
+
+  it('respects the "dryRun" option', () => {
+    mockEnv({
+      GITHUB_REF: 'refs/heads/run-dry',
+      GITHUB_SHA: 'bedface',
+      NPM_AUTH_TOKEN: 'secret'
+    })
+    mockFiles({
+      'package.json': {name: 'pkg', version: '1.0.0'}
+    })
+    return publish({dryRun: true}).then(() => {
+      expect(execa).toHaveBeenCalledTimes(0)
+    })
+  })
+
+  function mockEnv(env) {
+    restoreEnv = mockedEnv(env)
+  }
+})
--- src/publish.js
+++ src/publish.js
@@ -1,28 +1,20 @@
 const interpolate = require('interpolate')
-const readJSON = require('./read-json')
 const getContext = require('./get-context')
 const runDry = require('./run-dry')
+const {DEFAULT_MESSAGE} = require('./constants')
 
-module.exports = function publish(options, args = []) {
-  const {NPM_AUTH_TOKEN} = process.env
-  if (!NPM_AUTH_TOKEN) {
+module.exports = function publish(options = {}, npmArgs = []) {
+  if (!process.env.NPM_AUTH_TOKEN) {
     throw new Error(`You must set the NPM_AUTH_TOKEN environment variable`)
   }
 
-  const packageJson = readJSON('package.json')
-  const {name} = packageJson
+  const context = getContext(options)
+  const {version, tag} = context
+  const {message = DEFAULT_MESSAGE} = options
 
-  const run = options.dryRun ? require('execa') : runDry
-
-  return getContext(options).then(context => {
-    const {version, tag} = context
-    return run('npm', ['version', version, ...args])
-      .then(() => {
-        const message = interpolate(options.message, {name, version, tag})
-        return run('git', ['commit', '-m', message, 'package*.json'])
-      })
-      .then(() => {
-        return run('npm', ['publish', '--tag', tag, '--access', 'public'])
-      })
-  })
+  const run = options.dryRun ? runDry : require('execa')
+  return run('npm', [...npmArgs, 'version', version])
+    .then(() => run('git', ['commit', '-m', interpolate(message, context), 'package*.json']))
+    .then(() => run('npm', [...npmArgs, 'publish', '--tag', tag, '--access', 'public']))
+    .then(() => context)
 }
