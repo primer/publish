6d51a02080cf6e3c30811e7d78065aeeaaba970a
Shawn Allen <shawn.allen@github.com>
2019-02-06 11:03:56 -0800

    git: configure with sensible defaults

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+git config --global user.email "${GIT_USER_EMAIL:-design-systems@github.com}"
+git config --global user.name "${GIT_USER_NAME:-primer-bot}"
 sh -c "/primer-publish/cli.js $*"
