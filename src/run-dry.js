079aa8d2d431a519b5e81f0cc1cb1eaa953d334d
Shawn Allen <shawn.allen@github.com>
2019-02-06 09:53:33 -0800

    chore: improve dry-run output

--- src/run-dry.js
+++ src/run-dry.js
@@ -1,4 +1,4 @@
 module.exports = function runDry(cmd, args, execOpts = {}) {
-  console.warn(`[run] ${cmd} ${JSON.stringify(args)} (${JSON.stringify(execOpts)})`)
+  console.warn(`[dry-run] ${cmd} ${JSON.stringify(args)} (execOpts: ${JSON.stringify(execOpts)})`)
   return Promise.resolve({stdout: '', stderr: ''})
 }
