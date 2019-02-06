a5ac2898f3a0e6b8ffda12b0de4a2f4037ede81a
Shawn Allen <shawn.allen@github.com>
2019-02-06 10:56:34 -0800

    chore: lint (commenting out git commit stuff)

--- src/publish.js
+++ src/publish.js
@@ -1,8 +1,8 @@
 const actionStatus = require('action-status')
-const interpolate = require('interpolate')
+// const interpolate = require('interpolate')
 const getContext = require('./get-context')
 const runDry = require('./run-dry')
-const {DEFAULT_MESSAGE} = require('./constants')
+// const {DEFAULT_MESSAGE} = require('./constants')
 
 module.exports = function publish(options = {}, npmArgs = []) {
   if (!process.env.NPM_AUTH_TOKEN) {
@@ -11,35 +11,37 @@ module.exports = function publish(options = {}, npmArgs = []) {
 
   const context = getContext(options)
   const {name, version, tag} = context
-  const {message = DEFAULT_MESSAGE} = options
-  const filesToCommit = 'package*.json'
+  // const {message = DEFAULT_MESSAGE} = options
+  // const filesToCommit = 'package*.json'
 
   const run = options.dryRun ? runDry : require('execa')
   const execOpts = {stdio: 'inherit'}
-  return Promise.resolve()
-    .then(() =>
-      publishStatus({
-        state: 'pending',
-        description: `npm version ${version}`
-      })
-    )
-    .then(() => run('npm', [...npmArgs, 'version', version], execOpts))
-    // .then(() => run('git', ['commit', '-m', interpolate(message, context), filesToCommit], execOpts))
-    .then(() =>
-      publishStatus({
-        state: 'pending',
-        description: `npm publish --tag ${tag}`
-      })
-    )
-    .then(() => run('npm', [...npmArgs, 'publish', '--tag', tag, '--access', 'public'], execOpts))
-    .then(() =>
-      publishStatus({
-        state: 'success',
-        description: version,
-        url: `https://unpkg.com/${name}@${version}/`
-      })
-    )
-    .then(() => context)
+  return (
+    Promise.resolve()
+      .then(() =>
+        publishStatus({
+          state: 'pending',
+          description: `npm version ${version}`
+        })
+      )
+      .then(() => run('npm', [...npmArgs, 'version', version], execOpts))
+      // .then(() => run('git', ['commit', '-m', interpolate(message, context), filesToCommit], execOpts))
+      .then(() =>
+        publishStatus({
+          state: 'pending',
+          description: `npm publish --tag ${tag}`
+        })
+      )
+      .then(() => run('npm', [...npmArgs, 'publish', '--tag', tag, '--access', 'public'], execOpts))
+      .then(() =>
+        publishStatus({
+          state: 'success',
+          description: version,
+          url: `https://unpkg.com/${name}@${version}/`
+        })
+      )
+      .then(() => context)
+  )
 
   function publishStatus(props = {}) {
     return actionStatus(
