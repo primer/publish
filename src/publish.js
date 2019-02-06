386327f3c30d17754fd3b8a339e20a37eae5652b
Shawn Allen <shawn.allen@github.com>
2019-02-05 23:56:20 -0800

    resolve a promise in publish()

--- src/publish.js
+++ src/publish.js
@@ -13,7 +13,8 @@ module.exports = function publish(options = {}, npmArgs = []) {
   const {message = DEFAULT_MESSAGE} = options
 
   const run = options.dryRun ? runDry : require('execa')
-  return run('npm', [...npmArgs, 'version', version])
+  return Promise.resolve()
+    .then(() => run('npm', [...npmArgs, 'version', version]))
     .then(() => run('git', ['commit', '-m', interpolate(message, context), 'package*.json']))
     .then(() => run('npm', [...npmArgs, 'publish', '--tag', tag, '--access', 'public']))
     .then(() => context)
