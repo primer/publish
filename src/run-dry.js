393614e84efa43016d759fdd1c4d41344e4fab12
Shawn Allen <shawn.allen@github.com>
2019-02-05 23:46:29 -0800

    fix: tidy up dry-run console warnings

--- src/run-dry.js
+++ src/run-dry.js
@@ -1,4 +1,4 @@
-module.exports = function runDry(cmd, args, execOpts) {
-  console.warn(`[run] ${JSON.stringify(cmd)} ${JSON.stringify(args)} (${JSON.stringify(execOpts)})`)
+module.exports = function runDry(cmd, args, execOpts = {}) {
+  console.warn(`[run] ${cmd} ${JSON.stringify(args)} (${JSON.stringify(execOpts)})`)
   return Promise.resolve({stdout: '', stderr: ''})
 }
