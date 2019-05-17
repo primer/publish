60a603127b5dfe67d21dfadba8b0c200ab895e54
Emily Plummer <emplums@github.com>
2019-05-17 15:49:55 -0700

    update entry point to handle different auths

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-if [ -n "$NPM_AUTH_TOKEN" ]; then
+AUTH="${GPR_AUTH_TOKEN:=$NPM_AUTH_TOKEN}"
+
+if [ -z "$AUTH" ]; then
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
   NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
   NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
   NPM_REGISTRY_SCHEME="http"
 
   # Allow registry.npmjs.org to be overridden with an environment variable
-  printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
+  printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$AUTH" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
   chmod 0600 "$NPM_CONFIG_USERCONFIG"
 fi
 
