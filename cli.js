901bd0f7a7794057db4e8f71c4668e88a863db48
Emily Plummer <emplums@github.com>
2020-08-27 14:20:17 -0700

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,12 +1,31 @@
 #!/usr/bin/env node
 const publish = require('./src/publish')
 
+const yargs = require('yargs')
+  .option('dry-run', {
+    describe: 'Print what will be done without doing it',
+    type: 'boolean'
+  })
+  .option('dir', {
+    describe: 'Path to the directory that contains the package.json to publish',
+    type: 'string',
+    default: '.'
+  })
+  .alias('help', 'h')
+
+const options = yargs.argv
+
+if (options.help) {
+  yargs.showHelp()
+  process.exit(0)
+}
+
+console.log('options 🌈', options)
 const npmArgs = process.env.INPUT_NPM_ARGS
-const options = JSON.parse(process.env.INPUT_OPTIONS)
+delete options._
 
-
-console.log(`[publish] options🌈: ${options}`)
-console.log(`[publish] npm args🌈: ${npmArgs}`)
+console.warn(`[publish] options: ${JSON.stringify(options, null, 2)}`)
+console.warn(`[publish] npm args: ${npmArgs}`)
 
 publish(options, npmArgs)
   .then(context => {
